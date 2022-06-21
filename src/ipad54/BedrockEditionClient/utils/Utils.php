ae41d1d2b88ae710302e4e4a75080f37968318eb
LordBombardir <60620262+LordBombardir@users.noreply.github.com>
2022-06-21 16:44:33 +0300

    Поддержка русского языка

--- src/ipad54/BedrockEditionClient/utils/Utils.php
+++ src/ipad54/BedrockEditionClient/utils/Utils.php
@@ -12,16 +12,16 @@ class Utils{
 	private const PATH = "vendor/pocketmine/bedrock-data/required_item_list.json";
 
 	public static function makeItemTypeDictionary() : ItemTypeDictionary{
-		$data = PMUtils::assumeNotFalse(file_get_contents(self::PATH), "Missing required resource file");
+		$data = PMUtils::assumeNotFalse(file_get_contents(self::PATH), "Отсутствует требуемый файл ресурсов");
 		$table = json_decode($data, true);
 		if(!is_array($table)){
-			throw new AssumptionFailedError("Invalid item list format");
+			throw new AssumptionFailedError("Недопустимый формат списка предметов");
 		}
 
 		$params = [];
 		foreach($table as $name => $entry){
 			if(!is_array($entry) || !is_string($name) || !isset($entry["component_based"], $entry["runtime_id"]) || !is_bool($entry["component_based"]) || !is_int($entry["runtime_id"])){
-				throw new AssumptionFailedError("Invalid item list format");
+				throw new AssumptionFailedError("Недопустимый формат списка предметов");
 			}
 			$params[] = new ItemTypeEntry($name, $entry["runtime_id"], $entry["component_based"]);
 		}
